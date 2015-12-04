d2e00429ed1ab79f049ed9915299acac20507b88
AarushiJain ChristianBonacore <aarushichristian@appacademy.io>
2015-12-04 11:52:27 -0500

    Add populate_board to Board.

--- minesweeper.rb
+++ minesweeper.rb
@@ -8,19 +8,39 @@ class Board
     @grid = grid
   end
 
-  def get_adjacent_positions(pos)
-    #pos = [2, 3]
+  def populate_board
+    grid.each_with_index do |row, row_indx|
+      row.each_index do |square|
+        tile = Tile.new(self)
+        grid[row_indx][square] = tile
+      end
+    end
 
   end
+
+  def populate_bombs
+    num_bombs = rand(1..40)
+    num_bombs.times {}
+  end
+
+  def get_adjacent_positions(pos)
+    #pos = [2, 3]
+  end
+
 
 
 end
 
 class Tile
   attr_accessor :bomb, :revealed, :flagged
-  def initialize
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
     @bomb = false
     @revealed = false
     @flagged = false
   end
+
+
 end
