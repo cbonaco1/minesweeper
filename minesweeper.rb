50ae886d186b22df7766af85854f38e11765c7fd
AarushiJain ChristianBonacore <aarushichristian@appacademy.io>
2015-12-04 12:12:37 -0500

    Populate bombs.

--- minesweeper.rb
+++ minesweeper.rb
@@ -18,9 +18,29 @@ class Board
 
   end
 
+  # def [](row, col)
+  #  @grid[row][col]
+  # end
+
+
   def populate_bombs
     num_bombs = rand(1..40)
-    num_bombs.times {}
+    4.times do
+      pos = [rand(0...grid.length), rand(0...grid.length)]
+      current_tile = @grid[pos[0]][pos[1]]
+      current_tile.bomb = true
+    end
+  end
+
+  def bomb_count
+    count = 0
+    grid.each_with_index do |row, row_indx|
+      row.each_index do |square|
+        tile = grid[row_indx][square]
+        count += 1 if tile.bomb
+      end
+    end
+    count
   end
 
   def get_adjacent_positions(pos)
